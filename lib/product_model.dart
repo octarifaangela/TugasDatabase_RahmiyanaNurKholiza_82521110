708ee5ed2b482aa97ce94b245f212ce5f70e2c78
Octarifa Angela <octarifa.825210106@stu.untar.ac.id>
2023-12-05 10:42:37 +0700

    Initial commit

---
+++ lib/product_model.dart
@@ -0,0 +1,21 @@
+class ProductModel{
+  int? id;
+  String? name;
+  String? category;
+  String? createdAt;
+  String? updatedAt;
+
+
+  ProductModel({this.id, this.name, this.category, this.createdAt, this.updatedAt});
+
+  factory ProductModel.fromJson(Map<String, dynamic> json ) {
+    return ProductModel(
+      id: json['id'],
+      name: json['name'],
+      category: json['category'],
+      createdAt: json['createdAt'],
+      updatedAt: json['updatedAt'],
+    );
+  }
+
+}
